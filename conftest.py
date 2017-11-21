60ceecb81f1ba63de71186117f88f084cda2c326
Karlisson Bezerra <macedobz@gmail.com>
2017-11-21 14:57:00 -0200

    Remove useless fixture

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import random
-from dale.data.tokens import Token, TOKEN_TYPES
-
-
-@pytest.fixture
-def random_token():
-    token_type = random.choice(TOKEN_TYPES)
-    return token_type()
