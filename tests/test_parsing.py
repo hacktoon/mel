c4ed8d7d1829796a9be555ff7df29f1868cf4927
Karlisson Bezerra <macedobz@gmail.com>
2017-11-21 15:05:08 -0200

    Enhance value checking tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -4,31 +4,49 @@ from dale.data import tokens
 from dale.data.errors import LexingError, ParsingError
 
 
-@pytest.mark.parametrize('input, expected', [
+@pytest.mark.parametrize('test_input, expected', [
     ('56.75', 56.75),
     ('-0.75', -0.75),
     ('-.099999', -.099999),
     ('-0.75e10', -0.75e10),
     ('+1.45e-10', 1.45e-10),
+    ('true', True),
+    ('false', False),
     ('-56', -56),
     ('45', 45),
     ('"string"', 'string'),
     ("'string'", 'string'),
     ('@"/foo/bar"', '/foo/bar'),
+    ('@"/foo/\nbar"', '/foo/\nbar'),
     ('@foo.bar', ['foo', 'bar']),
 ])
-def test_parsing_of_single_values(input, expected):
-    tree = Parser(input).parse()
+def test_parsing_single_values(test_input, expected):
+    tree = Parser(test_input).parse()
     assert tree.value() == expected
 
 
-def test_simple_expression_parsing():
+@pytest.mark.parametrize('test_input', [
+    ('%foo'),
+    ('.bar'),
+    (':orphan-param'),
+    ('orphan-keyword'),
+    ('@/foo/bar'),
+])
+def test_parsing_invalid_single_values_will_raise_errors(test_input):
+    with pytest.raises(ParsingError):
+        Parser(test_input).parse()
+
+
+def test_parsing_simple_expression():
     tree = Parser('(name :id 1 "foo")').parse()
-    assert tree[0].keyword.value() == 'name'
-    assert tree[0].parameters.value() == {'id': 1}
+    assert tree.value() == {
+        'keyword': 'name',
+        'parameters': {'id': 1},
+        'values': 'foo'
+    }
 
 
-def test_expression_named_closing():
+def test_parsing_expression_with_named_closing():
     tree = Parser('(name :id 1 "foo")name)').parse()
     assert tree.value() == {
         'keyword': 'name',
@@ -37,7 +55,16 @@ def test_expression_named_closing():
     }
 
 
-def test_named_expression_closing_with_wrong_keyword():
+def test_parsing_expression_with_named_closing():
+    tree = Parser('(name :id 1 "foo")name)').parse()
+    assert tree.value() == {
+        'keyword': 'name',
+        'parameters': {'id': 1},
+        'values': 'foo'
+    }
+
+
+def test_parsing_expression_with_wrong_closing_keyword():
     with pytest.raises(ParsingError):
         Parser('(start  \t "foo")end)').parse()
 
