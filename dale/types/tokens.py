9e39eb1ab47ed77b67f372a39604f49df280cc72
Karlisson Bezerra <macedobz@gmail.com>
2017-12-07 15:49:17 -0200

    Remove data normalization from token classes

--- dale/types/tokens.py
+++ dale/types/tokens.py
@@ -1,160 +1,22 @@
 import re
-import codecs
-
-from .errors import LexingError
+from collections import OrderedDict
 
 
-NAME_RULE = r'[_a-zA-Z]\w*(-[_a-zA-Z]\w*)?'
-SINGLE_QUOTE_STRING = r"'(?:\\'|[^'])*'"
-DOUBLE_QUOTE_STRING = r'"(?:\\"|[^"])*"'
+rc = re.compile
 
-
-class Token:
-    id = ''
-    regex = ''
-    skip = False
-
-    def __init__(self, match='', index=0):
-        self.match = match
-        self.index = index
-
-    @property
-    def value(self, context=None):
-        return self.match
-
-    def __repr__(self):
-        return self.match
-
-    def __str__(self):
-        return self.match
-
-
-class WhitespaceToken(Token):
-    id = 'Whitespace'
-    regex = r'[,\s]+'
-    skip = True
-
-
-class CommentToken(Token):
-    id = 'Comment'
-    regex = r'#[^\n\r]*'
-    skip = True
-
-    @property
-    def value(self):
-        return self.match[1:]
-
-
-class DotToken(Token):
-    id = 'Dot'
-    regex = r'\.'
-
-
-class LeftBracketToken(Token):
-    id = 'LeftBracket'
-    regex = r'\['
-
-
-class RightBracketToken(Token):
-    id = 'RightBracket'
-    regex = r'\]'
-
-
-class LeftParenToken(Token):
-    id = 'LeftParen'
-    regex = r'\('
-
-
-class RightParenToken(Token):
-    id = 'RightParen'
-    regex = r'\)'
-
-
-class StringToken(Token):
-    id = 'String'
-    regex = '({}|{})'.format(SINGLE_QUOTE_STRING, DOUBLE_QUOTE_STRING)
-
-    @property
-    def value(self):
-        # thanks to @rspeer at https://stackoverflow.com/a/24519338/544184
-        ESCAPE_SEQUENCE_RE = re.compile(r'''
-            \\( U........    # 8-digit hex escapes
-            | u....          # 4-digit hex escapes
-            | x..            # 2-digit hex escapes
-            | [0-7]{1,3}     # Octal escapes
-            | N\{[^}]+\}     # Unicode characters by name
-            | [\\'"abfnrtv]  # Single-character escapes
-            )''', re.VERBOSE)
-
-        def decode_match(match):
-            return codecs.decode(match.group(0), 'unicode-escape')
-        return ESCAPE_SEQUENCE_RE.sub(decode_match, self.match[1:-1])
-
-
-class QueryToken(Token):
-    id = 'Query'
-    regex = '@'
-
-
-class BooleanToken(Token):
-    id = 'Boolean'
-    regex = 'true|false'
-
-    @property
-    def value(self):
-        return {'true': True, 'false': False}[self.match]
-
-
-class ParameterToken(Token):
-    id = 'Parameter'
-    regex = ':' + NAME_RULE
-
-    @property
-    def value(self):
-        return self.match[1:]
-
-
-class NameToken(Token):
-    id = 'Name'
-    regex = NAME_RULE
-
-
-class FloatToken(Token):
-    id = 'Float'
-    regex = r'[-+]?\d*\.\d+([eE][-+]?\d+)?\b'
-
-    @property
-    def value(self):
-        return float(self.match)
-
-
-class IntToken(Token):
-    id = 'Int'
-    regex = r'[-+]?\d+\b'
-
-    @property
-    def value(self):
-        return int(self.match)
-
-
-class EOFToken(Token):
-    id = 'end of file'
-
-
-# the order of tokens is important in this list
-TOKEN_TYPES = [
-    LeftBracketToken,
-    RightBracketToken,
-    LeftParenToken,
-    RightParenToken,
-    DotToken,
-    ParameterToken,
-    BooleanToken,
-    NameToken,
-    WhitespaceToken,
-    CommentToken,
-    QueryToken,
-    StringToken,
-    FloatToken,
-    IntToken
-]
+specs = OrderedDict([
+    ('.',          rc(r'\.')),
+    ('@',          rc(r'@')),
+    (':',          rc(r':')),
+    ('(',          rc(r'\(')),
+    (')',          rc(r'\)')),
+    ('[',          rc(r'\[')),
+    (']',          rc(r'\]')),
+    ('whitespace', rc(r'[,\s]+')),
+    ('comment',    rc(r'#[^\n\r]*')),
+    ('boolean',    rc(r'(true|false)\b')),
+    ('name',       rc(r'[_a-zA-Z]\w*(-[_a-zA-Z]\w*)?')),
+    ('float',      rc(r'[-+]?\d*\.\d+([eE][-+]?\d+)?\b')),
+    ('int',        rc(r'[-+]?\d+\b')),
+    ('string',     rc(r'|'.join([r"'(?:\\'|[^'])*'", r'"(?:\\"|[^"])*"'])))
+])
