6c1553a69da3bacfc7c5e3e57020de5886fa6813
Karlisson Bezerra <macedobz@gmail.com>
2018-01-12 00:21:14 -0200

    Organize code

--- dale/parsing.py
+++ dale/parsing.py
@@ -18,17 +18,14 @@ class Parser:
     def _parse_expression(self):
         return ExpressionParser(self.stream).parse()
 
-    def _parse_reference(self):
-        return ReferenceParser(self.stream).parse()
-
     def _parse_value(self):
         parser_method = {
             '@': self._parse_query,
             '<': self._parse_file,
             '$': self._parse_env,
             '[': self._parse_list,
-            'boolean': self._parse_boolean,
             'name': self._parse_reference,
+            'boolean': self._parse_boolean,
             'string': self._parse_string,
             'float': self._parse_float,
             'int': self._parse_int
@@ -39,14 +36,6 @@ class Parser:
         except KeyError:
             expected_values = list(parser_method.keys())
             raise UnexpectedTokenError(token, expected_values)
-        return node
-
-    def _parse_list(self):
-        node = nodes.ListNode()
-        self.stream.read('[')
-        while not self.stream.is_current(']'):
-            node.add(self._parse_value())
-        self.stream.read(']')
         return node
 
     def _parse_query(self):
@@ -67,6 +56,21 @@ class Parser:
         variable = self.stream.read('name')
         return nodes.EnvNode(variable)
 
+    def _parse_list(self):
+        node = nodes.ListNode()
+        self.stream.read('[')
+        while not self.stream.is_current(']'):
+            node.add(self._parse_value())
+        self.stream.read(']')
+        return node
+
+    def _parse_reference(self):
+        return ReferenceParser(self.stream).parse()
+
+    def _parse_boolean(self):
+        value = self.stream.read('boolean')
+        return nodes.BooleanNode(value)
+
     def _parse_string(self):
         value = self.stream.read('string')
         return nodes.StringNode(value)
@@ -78,10 +82,6 @@ class Parser:
     def _parse_int(self):
         value = self.stream.read('int')
         return nodes.IntNode(value)
-
-    def _parse_boolean(self):
-        value = self.stream.read('boolean')
-        return nodes.BooleanNode(value)
 
 
 class ReferenceParser(Parser):
