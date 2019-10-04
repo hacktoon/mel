11a5e20710d0eb890a17feb46c43a0692ff8ae32
Karlisson Bezerra <macedobz@gmail.com>
2019-10-04 01:27:00 -0300

    Simplify TokenStream

--- mel/lexing.py
+++ mel/lexing.py
@@ -1,4 +1,42 @@
-from . import tokens
+import re
+import tokens  # noqa
+
+from .exceptions import ParsingError
+
+
+_parsers = {}
+
+
+def token(id, regex=None):
+    regex = re.compile(regex or re.escape(id))
+
+    def parser(text, index=0):
+        match = regex.match(text, index)
+        if match:
+            return Token(id, match.group(0), match.span())
+        raise ParsingError
+    _parsers[id] = parser
+    return parser
+
+
+class Token:
+    def __init__(self, id, text, index):
+        self.id = id
+        self.text = text
+        self.index = index
+
+    def __eq__(self, token):
+        return self.id == token.id
+
+    def __len__(self):
+        return len(self.text)
+
+    def __repr__(self):
+        return "TOKEN({!r})".format(self.text)
+
+    def __str__(self):
+        start, end = self.index
+        return self.text[start:end]
 
 
 class TokenStream:
@@ -13,10 +51,7 @@ class TokenStream:
         self.index = index
 
     def parse(self, token_id):
-        parser = tokens.parsers[token_id]
+        parser = _parsers[token_id]
         token = parser(self.text, self.index)
-        self.index += len(token.text)
+        self.index += len(token)
         return token
-
-    def is_eof(self):
-        return self.index >= len(self.tokens)
--- mel/tokens.py
+++ mel/tokens.py
@@ -1,51 +1,11 @@
-import re
+from .lexing import token
 
-from .exceptions import ParsingError
-
-parsers = {}
-
-
-class Token:
-    def __init__(self, id, text, index):
-        self.id = id
-        self.text = text
-        self.index = index
-
-    def __eq__(self, token):
-        return self.id == token.id
-
-    def __len__(self):
-        return len(self.text)
-
-    def __repr__(self):
-        return "TOKEN({!r})".format(self.text)
-
-    def __str__(self):
-        start, end = self.index
-        return self.text[start:end]
-
-
-def token(id, regex=None, skip=False):
-    regex = re.compile(regex or re.escape(id))
-
-    def parser(text, index=0):
-        match = regex.match(text, index)
-        if match:
-            return Token(id, match.string, match.span())
-        raise ParsingError
-    parsers[id] = parser
-    return parser
-
-
-token("whitespace", r"([^\S\n\r]|;|,)+", skip=True)
-token("newline", r"\r?\n|\r", skip=True)
-token("comment", r"--[^\n\r]*", skip=True)
-
+token("space", r"(\s|;|,)*")
+token("comment", r"--[^\n\r]*")
 token("string", r"'[^']*'")
 token("template-string", r'"[^"]*"')
 token("float", r"-?\d*\.\d+([eE][-+]?\d+)?\b")
 token("int", r"-?\d+\b")
-token("boolean", r"([tT]rue|[fF]alse)\b")
 token("name", r"[a-z]\w*")
 token("concept", r"[A-Z]\w*")
 token("!=")
