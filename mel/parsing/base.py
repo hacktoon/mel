fb31d34362bdb841332257cb23b21bb3247ec777
Karlisson Bezerra <macedobz@gmail.com>
2019-10-09 01:42:52 -0300

    wip

--- mel/parsing/base.py
+++ mel/parsing/base.py
@@ -3,6 +3,9 @@ import functools
 from ..exceptions import ParsingError
 
 
+_rules = {}
+
+
 # decorator - add stream data to node instance via parser method
 def indexed(parse_method):
     @functools.wraps(parse_method)
@@ -127,3 +130,70 @@ class TokenParser(BaseParser):
         node = self.build_node()
         node.value = self.parse_token(self.Token)
         return node
+
+
+def root(alternatives):
+    _rules['root'] = alternatives
+
+
+def rule(id, alternatives):
+    _rules[id] = alternatives
+
+
+def zero_many(rule):
+    _rules[id] = rule
+
+
+def option(rules):
+    _rules[id] = rule
+
+
+def sequence(rules):
+    _rules[id] = rule
+
+
+# PARSING GRAMMAR
+# TODO: merge with token grammar
+
+# root(zero_many('expression'))
+
+# rule('expression', option('tag', 'relation', 'value'))
+
+# rule('relation', sequence('path', 'sign', 'value'))
+
+# rule('path', sequence('keyword',
+#   zero_many(sequence('separator', 'keyword'))
+# ))
+# rule('keyword', option('NAME', 'CONCEPT', 'meta-keyword'))
+# rule('meta-keyword', sequence(
+#     option('LOG', 'ALIAS', 'CACHE', 'FORMAT', 'META', 'DOC'),
+#     'NAME'
+# ))
+
+# rule('separator', option('SUB-PATH', 'META-PATH'))
+
+# rule('sign', option('EQUAL', 'DIFFERENT', 'LT',
+# 'LTE', 'GT', 'GTE', 'IN', 'NOT-IN'))
+
+# rule('value', option('reference', 'literal', 'list', 'object'))
+
+# rule('reference', sequence(
+#     'head-ref',
+#     zero_many(sequence('separator', 'sub-ref'))
+# ))
+# rule('head-ref', option('query', 'keyword'))
+# rule('sub-ref',
+#   option('RANGE', 'INT', 'tag', 'list',
+#   'object', 'query', 'keyword', 'wildcard'))
+
+# rule('literal', option('INT', 'FLOAT', 'STRING',
+#   'TEMPLATE-STRING', 'BOOLEAN'))
+
+# rule('list', sequence('[', zero_many('value'), ']'))
+
+# rule('object', sequence('(', 'object-key', zero_many('expression'), ')'))
+# rule('object-key',
+#   option('ANONYM-PATH', 'DEFAULT-FORMAT', 'DEFAULT-DOC', 'path'))
+
+# rule('query', sequence('{', 'query-key', zero_many('expression'), '}'))
+# rule('query-key', option('ANONYM-PATH', 'path'))
