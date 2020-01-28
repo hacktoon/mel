36d222cf0c94cb8660c2b0d6a14efbbfb8ad1d44
Karlisson Bezerra <macedobz@gmail.com>
2020-01-28 16:19:27 -0300

    Rename method

--- mel/parsing/__init__.py
+++ mel/parsing/__init__.py
@@ -30,13 +30,13 @@ class ZeroMany(Symbol):
         index = context.stream.save()
         try:
             for rule in self.rules:
-                node.add(rule.parser(context))
+                node.add(rule.parse(context))
         except ParsingError as error:
             context.stream.restore(index)
             raise error
         return node
 
-    def parser(self, context):
+    def parse(self, context):
         node = ZeroManyNode()
         while True:
             try:
@@ -52,12 +52,12 @@ class Seq(Symbol):
     def __init__(self, *rules):
         self.rules = rules
 
-    def parser(self, context):
+    def parse(self, context):
         node = SequenceNode()
         index = context.stream.save()
         try:
             for rule in self.rules:
-                node.add(rule.parser(context))
+                node.add(rule.parse(context))
         except ParsingError as error:
             context.stream.restore(index)
             raise error
@@ -70,12 +70,12 @@ class OneMany(Symbol):
     def __init__(self, *rules):
         self.rules = rules
 
-    def parser(self, context):
+    def parse(self, context):
         node = OneManyNode()
         while True:
             try:
                 for rule in self.rules:
-                    node.add(rule.parser(context))
+                    node.add(rule.parse(context))
             except ParsingError:
                 break
         if len(node):  # TODO: remove this check
@@ -90,11 +90,11 @@ class OneOf(Symbol):
         self.rules = rules
         # TODO: use `rules` to build error messages
 
-    def parser(self, context):
+    def parse(self, context):
         node = OneOfNode()
         for rule in self.rules:
             try:
-                node.add(rule.parser(context))
+                node.add(rule.parse(context))
                 return node
             except ParsingError:
                 pass
@@ -107,10 +107,10 @@ class Opt(Symbol):
     def __init__(self, rule):
         self.rule = rule
 
-    def parser(self, context):
+    def parse(self, context):
         node = OptionalNode()
         try:
-            node.add(self.rule.parser(context))
+            node.add(self.rule.parse(context))
         except ParsingError:
             pass
         return node
@@ -122,12 +122,12 @@ class Rule(Symbol):
     def __init__(self, id):
         self.id = id
 
-    def parser(self, context):
+    def parse(self, context):
         rule = context.symbols[id]
         node = RuleNode(id)
         index = context.stream.save()
         try:
-            node.add(rule.parser(context))
+            node.add(rule.parse(context))
         except ParsingError as error:
             context.stream.restore(index)
             raise error
@@ -140,7 +140,7 @@ class Str(Symbol):
     def __init__(self, string):
         self.string = string
 
-    def parser(self, context):
+    def parse(self, context):
         context._skip_symbols(context.stream)
         text, index = context.stream.read_string(self.string)
         return StringNode(text, index)
@@ -149,7 +149,7 @@ class Str(Symbol):
 # REGEX SYMBOL ==========================================
 
 class Regex(Str):
-    def parser(self, context):
+    def parse(self, context):
         context._skip_symbols(context.stream)
         text, index = context.stream.read_pattern(self.string)
         return PatternNode(text, index)
@@ -181,7 +181,7 @@ class Grammar:
             stream=stream
         )
         tree = RootNode()
-        tree.add(rule.parser(context))
+        tree.add(rule.parse(context))
         self._skip_symbols(context)
         if not stream.eof:
             raise ParsingError
@@ -199,6 +199,6 @@ class Grammar:
     def _skip_symbols(self, context):
         rules = self.skip_symbols.values()
         while True:
-            skipped = [True for rule in rules if rule.parser(context)]
+            skipped = [True for rule in rules if rule.parse(context)]
             if len(skipped) == 0:
                 break
