006c0283e7f8e5e362dc2dedec2b900417b78893
Karlisson Bezerra <macedobz@gmail.com>
2020-05-01 00:52:14 -0300

    Add digit test

--- tests/test_chars.py
+++ tests/test_chars.py
@@ -1,12 +1,28 @@
 from infiniscribe.parsing.chars import parse
-from infiniscribe.parsing.chars import LOWER
+from infiniscribe.parsing.chars import (
+    LOWER, UPPER, DIGIT
+)
+
+
+def fixture_chars(chars):
+    return list(parse(chars))
 
 
 def test_empty_text():
-    chars = list(parse(''))
+    chars = fixture_chars('')
     assert len(chars) == 0
 
 
-def test_lower_letter():
-    chars = list(parse('a'))
+def test_lower_letters():
+    chars = fixture_chars('a')
     assert chars[0].type == LOWER
+
+
+def test_upper_letters():
+    chars = fixture_chars('Y')
+    assert chars[0].type == UPPER
+
+
+def test_digits():
+    chars = fixture_chars('5')
+    assert chars[0].type == DIGIT
